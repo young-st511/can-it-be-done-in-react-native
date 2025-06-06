8b7fabba3ee777babe7101faa4b3ec7ad42a0f60
young-st511 <asdk511@gmail.com>
2025-06-06 14:52:02 +0900

    feat: Ring 기본 추가

--- bonuses/_boilerPlates/skia-examples/src/Rings/Ring.tsx
+++ bonuses/_boilerPlates/skia-examples/src/Rings/Ring.tsx
@@ -23,13 +23,12 @@ import { frag } from "../components";
 const source = frag`
 uniform shader image;
 
-
 vec2 rotate(in vec2 coord, in float angle, vec2 origin) {
   vec2 coord1 = coord - origin;
   vec2 rotated = coord1 * mat2( cos(angle), -sin(angle),
-                       sin(angle),  cos(angle));
+                                sin(angle),  cos(angle));
   return rotated + origin;
- }
+}
 
 vec4 main(vec2 xy) {
   return image.eval(xy);
@@ -37,7 +36,7 @@ vec4 main(vec2 xy) {
 `;
 
 const fromCircle = (center: Vector, r: number) => {
-  "worklet";
+  'worklet';
   return Skia.XYWHRect(center.x - r, center.y - r, r * 2, r * 2);
 };
 
@@ -54,14 +53,37 @@ interface RingProps {
   strokeWidth: number;
 }
 
-export const Ring = ({
-  center,
-  strokeWidth,
-  ring: { size, background, totalProgress, colors },
-}: RingProps) => {
+export const Ring = ({ center, strokeWidth, ring: { size, background, totalProgress, colors } }: RingProps) => {
+  const emptyRadius = size / 2 - strokeWidth / 2;
+
+  const clip = useMemo(() => {
+    const outerCircle = Skia.Path.Make();
+    outerCircle.addCircle(center.x, center.y, size / 2);
+
+    const innerCircle = Skia.Path.Make();
+    innerCircle.addCircle(center.x, center.y, size / 2 - strokeWidth);
+
+    return Skia.Path.MakeFromOp(outerCircle, innerCircle, PathOp.Difference);
+  }, [center.x, center.y, size]);
+
+  const fullPath = useMemo(() => {
+    const path = Skia.Path.Make();
+    const fullRevolutions = Math.floor(totalProgress);
+    for (let i = 0; i < fullRevolutions; i++) {
+      path.addCircle(center.x, center.y, emptyRadius);
+    }
+    path.addArc(fromCircle(center, emptyRadius), 0, 360 * (totalProgress % 1));
+    return path;
+  }, []);
+
+  if (!clip) return null;
+
   return (
-    <Group>
-      <Fill color={background} />
+    <Group transform={[{ rotate: -Math.PI / 2 }]} origin={center}>
+      <Group clip={clip}>
+        <Fill color={background} />
+        <Path path={fullPath} strokeWidth={strokeWidth} style={'stroke'} color={colors[0]} />
+      </Group>
     </Group>
   );
 };
